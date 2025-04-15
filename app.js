59cd9eb2bcb3aaf7a27149236ee61630fb633431
Antti Ollikkala <antti@neksim.com>
2025-04-15 07:20:38 +0300

    test throwing health endpoint

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
 })
 
 app.listen(PORT, () => {
